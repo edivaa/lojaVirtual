91e4985f7c128cd2f69fc62eb509bf3894618cb8
Ediva Goncalves Teodozio <edivaa@gmail.com>
2018-02-27 11:03:09 -0300

    Criaço de controleProdutos

---
+++ Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private ProdutosRepositorio _produtosRepositorio;
+
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            _produtosRepositorio = new ProdutosRepositorio();
+            var produtos = _produtosRepositorio.Produtos;
+
+            return View(produtos);
+        }
+    }
+}
