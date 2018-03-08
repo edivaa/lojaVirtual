944da5e49ad866a6bc5711990723954970a4f040
Ediva Goncalves Teodozio <edivaa@gmail.com>
2018-03-08 07:46:06 -0300

    Teste rodando

---
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private ProdutosRepositorio _produtosRepositorio;
+
+        // GET: Vitrine
+        public ActionResult Index()
+        {
+
+            _produtosRepositorio = new ProdutosRepositorio();
+            var produtos = _produtosRepositorio.Produtos;
+            return View();
+        }
+    }
+}
