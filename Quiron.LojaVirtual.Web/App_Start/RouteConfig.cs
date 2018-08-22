d2833d8fe1aedc870b2a063ee4169844bfa49f8d
Ediva Goncalves Teodozio <edivaa@gmail.com>
2018-08-21 22:47:49 -0300

    Ajuste das rotas

--- Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
+++ Quiron.LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -15,17 +15,43 @@ namespace Quiron.LojaVirtual.Web
 
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name:null,
-                url: "Pagina{pagina}",
-                defaults: new { controller= "Vitrine", action="ListaProdutos" }
+            //1 rota
+            routes.MapRoute(null,
+               "",
+               new { controller = "Vitrine",
+                     Action = "ListaProdutos",
+                     categoria = (string)null ,  pagina = "1" }
+            );
+
+            //2 rota
+            routes.MapRoute(null,
+              "Pagina{pagina}",
+              new { controller = "Vitrine",
+                    Action = "ListaProdutos",
+                    categoria = (string)null }, new { pagina = @"\d+" }
+            );
+
+            //3 rota 
+            routes.MapRoute(  null,"{categoria}", 
+                              new {
+                                  controller ="Vitrine",
+                                  action ="ListaProdutos", pagina=1}
                 );
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Vitrine", action = "ListaProdutos", id = UrlParameter.Optional }
-            );
+            //4 rota
+            routes.MapRoute(null,
+                       "{categoria} Pagina{pagina}",
+                       new
+                       {
+                           controller = "Vitrine",
+                           Action = "ListaProdutos"
+                       },
+                       new { pagina = @"\d+" }
+                     );
+
+            routes.MapRoute(null, "{controller};{action}");
+
+
         }
     }
 }
