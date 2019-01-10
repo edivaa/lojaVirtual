aaa0eccb98247d5336260d4b9c9cadb7c8f0acda
Edivã Gonçalves <edivaa@gmail.com>
2019-01-09 23:24:09 -0300

    Carrinho ItemCarrinho

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Carrinho.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    
+    class Carrinho
+    {
+        private readonly List<ItemCarrinho> _itemCarrinho = new List<ItemCarrinho>();
+
+        public void AdicionarItem(Produto produto, int quantidade)
+        {
+            ItemCarrinho item = _itemCarrinho.FirstOrDefault(p => p.Produto.ProdutoId == produto.ProdutoId);
+
+            if (item == null)
+            {
+                _itemCarrinho.Add(new ItemCarrinho
+                {
+                    Produto = produto,
+                    Quantidade = quantidade
+                });
+            }else
+            {
+                item.Quantidade += quantidade;
+            }
+
+        }
+
+        public void RemoveItem(Produto produto)
+        {
+            _itemCarrinho.RemoveAll(l => l.Produto.ProdutoId == produto.ProdutoId);
+        }
+
+
+        public decimal ObterValorTotal()
+        {
+            return _itemCarrinho.Sum(e => e.Produto.Preco * e.Quantidade);
+        }
+
+        
+    }
+
+    public class ItemCarrinho
+    {
+
+        public Produto Produto { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
